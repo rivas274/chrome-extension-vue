19e176ce6496cb45795ef209c13b9d65f308c220
rivas274 <rivas274@outlook.com>
2016-07-06 00:55:30 -0400

    time

--- js/options.js
+++ js/options.js
@@ -12,11 +12,10 @@ new Vue({
 			var h= this.tiempo.hora;
 			var m= this.tiempo.minuto;
 			var s= this.tiempo.segundo;
-			h=(m<60) ? h : (h+1);
-			m=(s<60) ? m : (m+1);
-			s=(s<60) ? (s+1) : 0;
+			h=(m <= 59) ? h : (h+1);
+			m=(s <= 59) ? m : (m+1);
+			s=(s <= 59) ? (s+1) : 0;
 			this.tiempo = { hora: h , minuto: m, segundo: s};
-			console.log(JSON.stringify(this.tiempo));
 			if(this.tiempo.minuto<2){
 				setTimeout(function(){
 					this.cronometro();
