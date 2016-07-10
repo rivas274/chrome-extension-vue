bae1b38828725082f1a7bec5849f73ef98bb88db
rivas274 <rivas274@outlook.com>
2016-07-09 22:56:08 -0400

    component

--- js/options.js
+++ js/options.js
@@ -1,9 +1,7 @@
-var option=new Vue({
-    el:".container-fluid",
-    data:{
-		contador:{hora:"00:00:00",time:0,contar:false},
-    },
-    methods: {
+Vue.component('cronometro',{
+	template:"#cronometro",
+	props: {'contador':{}},
+	methods: {
 		temporizador: function () {
 			this.contador.time=moment(this.contador.hora, "H[h]:mm[m]:ss[s]");
 			if(!this.contador.contar){
@@ -17,9 +15,49 @@ var option=new Vue({
 			/*	time=moment(this.contador.time).add(1, 's');*/
 				this.contador.time=time;
 				this.contador.hora=moment(time).format('HH:mm:ss');
-				this.cronometro();
+				if(this.contador.contar){
+					this.cronometro();
+				}
 			}.bind(this), 1000);
-		}
+		},
+		detener: function () {
+			this.contador.contar=false;
+		},
+		borrarhijo() {
+			/*this.$dispatch(this.contador);*/
+			console.log(this.contador);
+            this.$parent.borrar(this.contador);
+        }
+	},
+})
+var option=new Vue({
+    el:".container-fluid",
+    data:{
+		contador_new:{
+				hora:"00:00:00",
+				time:0,
+				contar:false,
+				tipo:'+'
+			},
+		contador:[],
+    },
+    methods: {
+		agregar: function (contador) {
+			this.contador.push(contador);
+			this.limpiar();
+		},
+		limpiar: function () {
+			this.contador_new={
+				hora:"00:00:00",
+				time:0,
+				contar:false,
+				tipo:'+'
+			}
+		},
+		borrar(contador) {
+			console.log(this.contador+contador);
+			this.contador.$remove(contador);
+        }
 	},
   	computed: {
 	},
